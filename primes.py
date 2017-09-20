defb0b68ffd4234f2fa008c3160a5edd7ebc14b4
Evan Walker <walke208@mail.chapman.edu>
2017-09-20 04:33:34 +0000

    added example gen fxn

--- primes.py
+++ primes.py
@@ -53,3 +53,25 @@ eratosthenes(100)
 #    import sys
 #    eratosthenes(int(sys.argv[1]))
 
+
+#example of generators
+'''
+def fib_gen():
+    a = 1
+    b = 0
+    hold = 0
+    while True:
+        yield a
+        hold = a
+        a = a + b
+        b = hold
+
+
+def fibonacci(n):
+    my_list = []
+    g = fib_gen()
+    for i in range(n):
+        my_list.append(next(g))
+    #print(my_list)
+    return my_list
+'''
