94266631df1196450b9ebf4568f48592023ddf3a
Evan Walker <walke208@mail.chapman.edu>
2017-09-20 04:06:48 +0000

    modifiedprimes

--- primes.py
+++ primes.py
@@ -1,33 +1,49 @@
 #!/usr/bin/env python3
-#.remove(n)
+
+###
+# Name: Evan A Walker, Kristaaaa, Ehsan
+# Student ID: 01932978 , kristaIDHere, EhsanIDHere
+# Email: walke208@mail.chapman.edu , kristaEmailHere , ehsanEmailHere
+# Course: CS510 Fall 2017
+# Assignment: Classwork 4
+###
+
+
 import sys
 
 
 def eratosthenes(n):
-    try:
-        my_list = []
-        for i in range(n):
-            my_list.append(i)
-        my_list.remove(0)
-        my_list.remove(1)
-        multiple = 2
-        while multiple <= n:
-            count = 2
-            while count <= n/multiple:
-                if count*multiple in my_list:
-                    my_list.remove(count*multiple)
-                count = count + 1
-            multiple = multiple + 1
-        print(my_list)
-        return my_list
-    except Exception as e:
-        print(e)
+    assert n>1
+    my_list = []
+    for i in range(n):
+        my_list.append(i)
+    my_list.remove(0)
+    my_list.remove(1)
+    multiple = 2
+    while multiple <= n/multiple:
+        count = 2
+        while count <= n/multiple:
+            if count*multiple in my_list:
+                my_list.remove(count*multiple)
+            count = count + 1
+        multiple = multiple + 1
+    #print(my_list)
+    return my_list
+
 
 
 
 eratosthenes(120)
 
-#if __name__ == "__main__":
-#        import sys
-#        eratosthenes(int(sys.argv[1]))
+def gen_eratosthenes():
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    eratosthenes(int(sys.argv[1]))
 
