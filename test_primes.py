e3e03b370dbd6a53b27cdcd2358913b02463f75c
Evan Walker <walke208@mail.chapman.edu>
2017-09-20 04:34:35 +0000

    created testprimes file

---
+++ test_primes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+###
+# Name: Evan A Walker, Kristaaaa, Ehsan
+# Student ID: 01932978 , kristaIDHere, EhsanIDHere
+# Email: walke208@mail.chapman.edu , kristaEmailHere , ehsanEmailHere
+# Course: CS510 Fall 2017
+# Assignment: Classwork 4
+###
+
+
+import sys
+
+def test_primes():
+    
