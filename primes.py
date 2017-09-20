21e49404487fc74891150cceebca0179f3dc7320
Evan Walker <walke208@mail.chapman.edu>
2017-09-20 04:19:23 +0000

    docstring edit

--- primes.py
+++ primes.py
@@ -13,37 +13,43 @@ import sys
 
 
 def eratosthenes(n):
+    """
+    Args:
+        n (int): positive integer parameter. where n>1
+    Returns:
+        prime_list (list): a list of all prime numbers less than n
+    """
     assert n>1
-    my_list = []
+    prime_list = []
     for i in range(n):
-        my_list.append(i)
-    my_list.remove(0)
-    my_list.remove(1)
+        prime_list.append(i)
+    prime_list.remove(0)
+    prime_list.remove(1)
     multiple = 2
     while multiple <= n/multiple:
         count = 2
         while count <= n/multiple:
             if count*multiple in my_list:
-                my_list.remove(count*multiple)
+                prime_list.remove(count*multiple)
             count = count + 1
         multiple = multiple + 1
-    #print(my_list)
-    return my_list
+    #print(prime_list)
+    return prime_list
 
 
 
 
-eratosthenes(120)
+eratosthenes(1)
 
-def gen_eratosthenes():
-
-
+#def gen_eratosthenes():
 
 
 
 
 
-if __name__ == "__main__":
-    import sys
-    eratosthenes(int(sys.argv[1]))
+
+
+#if __name__ == "__main__":
+#    import sys
+#    eratosthenes(int(sys.argv[1]))
 
